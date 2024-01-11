f770c7e04997818cb5e5afdc81a6fc8bf3487114
animesh-77 <animeshsrivasta77@gmail.com>
2024-01-11 01:29:33 +0000

    refactor: change save directories

--- tutorials/img_cls/train_pt.py
+++ tutorials/img_cls/train_pt.py
@@ -10,39 +10,60 @@ from PIL import Image
 
 from network_pt import Net
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     ## cifar-10 dataset
     transform = transforms.Compose(
-        [transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    )
 
     batch_size = 20
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
-    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    trainset = torchvision.datasets.CIFAR10(
+        root="./data", train=True, download=True, transform=transform
+    )
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=batch_size, shuffle=True, num_workers=2
+    )
+    classes = (
+        "plane",
+        "car",
+        "bird",
+        "cat",
+        "deer",
+        "dog",
+        "frog",
+        "horse",
+        "ship",
+        "truck",
+    )
 
     # example images
     dataiter = iter(trainloader)
-    images, labels = next(dataiter) # note: for pytorch versions (<1.14) use dataiter.next()
+    images, labels = next(
+        dataiter
+    )  # note: for pytorch versions (<1.14) use dataiter.next()
 
-    im = Image.fromarray((torch.cat(images.split(1,0),3).squeeze()/2*255+.5*255).permute(1,2,0).numpy().astype('uint8'))
-    im.save("train_pt_images.jpg")
-    print('train_pt_images.jpg saved.')
-    print('Ground truth labels:' + ' '.join('%5s' % classes[labels[j]] for j in range(batch_size)))
-
+    im = Image.fromarray(
+        (torch.cat(images.split(1, 0), 3).squeeze() / 2 * 255 + 0.5 * 255)
+        .permute(1, 2, 0)
+        .numpy()
+        .astype("uint8")
+    )
+    im.save("tutorials/img_cls/train_pt_images.jpg")
+    print("train_pt_images.jpg saved.")
+    print(
+        "Ground truth labels:"
+        + " ".join("%5s" % classes[labels[j]] for j in range(batch_size))
+    )
 
     ## cnn
     net = Net()
-
 
     ## loss and optimiser
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-
     ## train
     for epoch in range(2):  # loop over the dataset multiple times
-
         running_loss = 0.0
         for i, data in enumerate(trainloader, 0):
             # get the inputs; data is a list of [inputs, labels]
@@ -59,13 +80,12 @@ if __name__ == '__main__':
 
             # print statistics
             running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' %
-                    (epoch + 1, i + 1, running_loss / 2000))
+            if i % 2000 == 1999:  # print every 2000 mini-batches
+                print("[%d, %5d] loss: %.3f" % (epoch + 1, i + 1, running_loss / 2000))
                 running_loss = 0.0
 
-    print('Training done.')
+    print("Training done.")
 
     # save trained model
-    torch.save(net.state_dict(), 'saved_model.pt')
-    print('Model saved.')
+    torch.save(net.state_dict(), "tutorials/img_cls/saved_model.pt")
+    print("Model saved.")
