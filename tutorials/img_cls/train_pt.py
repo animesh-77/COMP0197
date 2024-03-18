71a9a97158638733182c0905e60448bae85ae000
animesh-77 <animeshsrivastava77@gmail.com>
2024-03-18 12:39:43 +0000

    reafctor: resolve mypy/flake8 errors

--- tutorials/img_cls/train_pt.py
+++ tutorials/img_cls/train_pt.py
@@ -11,9 +11,18 @@ from PIL import Image
 from network_pt import Net
 
 if __name__ == "__main__":
+<<<<<<< Updated upstream
     ## cifar-10 dataset
     transform = transforms.Compose(
         [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+=======
+    # cifar-10 dataset
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]
+>>>>>>> Stashed changes
     )
 
     batch_size = 20
@@ -41,6 +50,7 @@ if __name__ == "__main__":
     images, labels = next(
         dataiter
     )  # note: for pytorch versions (<1.14) use dataiter.next()
+<<<<<<< Updated upstream
 
     im = Image.fromarray(
         (torch.cat(images.split(1, 0), 3).squeeze() / 2 * 255 + 0.5 * 255)
@@ -63,6 +73,30 @@ if __name__ == "__main__":
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
     ## train
+=======
+
+    im = Image.fromarray(
+        (torch.cat(images.split(1, 0), 3).squeeze() / 2 * 255 + 0.5 * 255)
+        .permute(1, 2, 0)
+        .numpy()
+        .astype("uint8")
+    )
+    im.save("train_pt_images.jpg")
+    print("train_pt_images.jpg saved.")
+    print(
+        "Ground truth labels:"
+        + " ".join("%5s" % classes[labels[j]] for j in range(batch_size))
+    )
+
+    # cnn
+    net = Net()
+
+    # loss and optimiser
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+    # train
+>>>>>>> Stashed changes
     for epoch in range(2):  # loop over the dataset multiple times
         running_loss = 0.0
         for i, data in enumerate(trainloader, 0):
@@ -81,11 +115,22 @@ if __name__ == "__main__":
             # print statistics
             running_loss += loss.item()
             if i % 2000 == 1999:  # print every 2000 mini-batches
+<<<<<<< Updated upstream
                 print("[%d, %5d] loss: %.3f" % (epoch + 1, i + 1, running_loss / 2000))
+=======
+                print(
+                    "[%d, %5d] loss: %.3f"
+                    % (epoch + 1, i + 1, running_loss / 2000)
+                )
+>>>>>>> Stashed changes
                 running_loss = 0.0
 
     print("Training done.")
 
     # save trained model
+<<<<<<< Updated upstream
     torch.save(net.state_dict(), "tutorials/img_cls/saved_model.pt")
+=======
+    torch.save(net.state_dict(), "saved_model.pt")
+>>>>>>> Stashed changes
     print("Model saved.")
